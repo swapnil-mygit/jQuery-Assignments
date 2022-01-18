06ea09827ef75133e3ea54248ac1564857204138
Swapnil <swapnilyende.perficient@gmail.com>
2022-01-18 18:50:34 +0530

    added link file

--- 1. Tab Assignment/myjQuery.js
+++ 1. Tab Assignment/myjQuery.js
@@ -1,19 +1,29 @@
 
 $(document).ready(function () {
+    $("#details").addClass("active");
     $("#para2").hide();
     $("#para3").hide();
     $("#para1").show();
     $('#details').click(function () {
+        $("#details").addClass("active");
+        $("#about").removeClass("active");
+        $("#assignment").removeClass("active");
         $("#para1").fadeIn();
         $("#para2").hide();
         $("#para3").hide();
     });
     $('#about').click(function () {
+        $("#details").removeClass("active");
+        $("#about").addClass("active");
+        $("#assignment").removeClass("active");
         $("#para1").hide();
         $("#para2").fadeIn();
         $("#para3").hide();
     });
     $('#assignment').click(function () {
+        $("#details").removeClass("active");
+        $("#about").removeClass("active");
+        $("#assignment").addClass("active");
         $("#para1").hide();
         $("#para2").hide();
         $("#para3").fadeIn();
